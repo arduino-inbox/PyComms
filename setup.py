b3bf30ea4d3af812fb923e9a5df10597d8bbf721
Roman Stets <romko.stets@gmail.com>
2014-05-07 17:53:34 +0300

    Setup.

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from distutils.core import setup
 setup(
     name='PyComms',
     version='0.0.0',
-    packages=['PyComms', 'MPU6050', ],
+    packages=['pycomms', 'mpu6050', 'hmc5883l', 'pca9685', 'bmp085', ],
     description='PyComms',
 )
