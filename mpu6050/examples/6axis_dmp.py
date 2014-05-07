e1b3352776ad8b1aa55f09a782e5a1ccba480d53
Roman Stets <romko.stets@gmail.com>
2014-05-07 18:07:03 +0300

    Test dmpGetAccel.

--- mpu6050/examples/6axis_dmp.py
+++ mpu6050/examples/6axis_dmp.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-
 import math
 import mpu6050
 
@@ -15,18 +14,15 @@ while True:
     # Get INT_STATUS byte
     mpuIntStatus = mpu.getIntStatus()
 
-    if mpuIntStatus >= 2: # check for DMP data ready interrupt (this should
-    # happen frequently)
+    if mpuIntStatus >= 2:  # check for DMP data ready interrupt
         # get current FIFO count
         fifoCount = mpu.getFIFOCount()
 
-        # check for overflow (this should never happen unless our code is
-        # too inefficient)
+        # check for overflow
         if fifoCount == 1024:
             # reset so we can continue cleanly
             mpu.resetFIFO()
             print('FIFO overflow!')
-
 
         # wait for correct available data length, should be a VERY short wait
         fifoCount = mpu.getFIFOCount()
@@ -37,12 +33,16 @@ while True:
         q = mpu.dmpGetQuaternion(result)
         g = mpu.dmpGetGravity(q)
         ypr = mpu.dmpGetYawPitchRoll(q, g)
+        a = mpu.dmpGetAccel(result)
 
-        print(ypr['yaw'] * 180 / math.pi),
-        print(ypr['pitch'] * 180 / math.pi),
-        print(ypr['roll'] * 180 / math.pi)
+        print(int(ypr['yaw'] * 180 / math.pi)),
+        print(int(ypr['pitch'] * 180 / math.pi)),
+        print(int(ypr['roll'] * 180 / math.pi)),
+        print(int(a['x'] * 9.8)),
+        print(int(a['y'] * 9.8)),
+        print(int(a['z'] * 9.8))
 
         # track FIFO count here in case there is > 1 packet available
         # (this lets us immediately read more without waiting for an
         # interrupt)
-        fifoCount -= packetSize  
+        fifoCount -= packetSize
